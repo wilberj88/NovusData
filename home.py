bf5e1de7cc246c1f6710cb7f205b41a1147e3737
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-02-27 09:44:41 -0500

    Create home.py

---
+++ home.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pydeck as pdk
+
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="Novus Data", page_icon="⚛️")
+
+st.title('Novus Data ⚛️')
+st.header("Tecnología para Gobernar tus Datos al instante ⚡")
+
+st.write("Configura y disfruta ahora ⏱️")
+
+st.markdown(
+  """
+  En esta web encontrarás:
+  - 🔎 _    Titularidad
+  - 🛒 _    Ciclo de Vida
+  - ✍️ _     Arquitectura
+  - ✍️ _     Modelación
+  - ✍️ _     Integración
+  - ✍️ _     Operación
+  - ✍️ _     Seguridad
+  - ✍️ _     Privacidad
+  - ✍️ _     Conciliación
+  - ✍️ _     Referentes
+  - ✍️ _     Lago
+  - ✍️ _     Elementos Críticos
+  - ✍️ _     Metadata
+  - ✍️ _     Calidad
+  - ✍️ _     Integración
+  - ✍️ _     Contenido
+  
+  Todo lo anterior basado en:
+  - Tecnología para la interoperabilidad
+  
+  EMPIEZA TU 🔎 DIAGNÓSTICO AHORA 🕰
+  """
+)
